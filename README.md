b787212f3273ffefefd9249e69471281449db897
Tishana <tishana.trainor@gmail.com>
2023-01-17 16:52:11 -0500

    add and update README

---
+++ README.md
@@ -0,0 +1,4 @@
+# Express Todo App
+
+This app works best with [react-todo28](https://github.com/tishana/reactTodo28) 
+
--- utilities/data.js
+++ utilities/data.js
@@ -1,6 +1,6 @@
 const todoData = [
     {description: 'Eat Healthy', complete: false},
-    {description: 'Work Out', complete: false},
+    {description: 'Work Out', complete: true},
     {description: 'Get Smart', complete: false}
 ]
 
